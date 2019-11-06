de0cbd6f61ee8cac5c650f53f78583e73dbb1ffb
Alex <devmobalex@gmail.com>
2019-11-06 15:16:04 +0100

    Register Finished

--- Webapp/src/app/pages/Member/register/register.component.ts
+++ Webapp/src/app/pages/Member/register/register.component.ts
@@ -34,6 +34,19 @@ export class RegisterComponent implements OnInit {
   }
 
   onFormSubmit(){
-    console.log(this.localMembre);
+    this.srv.registerMembre(this.localMembre).subscribe(data => {
+      console.log('ok');
+    });
+  }
+
+  onAddressChanged($event: IMembre) {
+    this.localMembre.Home_street = $event.Home_street;
+    this.localMembre.Home_num = $event.Home_num;
+    this.localMembre.Home_box = $event.Home_box;
+    this.localMembre.Home_Country_Name = $event.Home_Country_Name;
+    this.localMembre.Home_Country_id = $event.Home_Country_id;
+    this.localMembre.Home_City_Name = $event.Home_City_Name;
+    this.localMembre.Home_City_Zip = $event.Home_City_Zip;
+    this.localMembre.Home_city_id = $event.Home_city_id;
   }
 }
