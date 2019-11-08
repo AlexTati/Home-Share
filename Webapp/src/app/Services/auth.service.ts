f368a62320f8673ddddbc27734cdfa629a3a465a
s4my <samseche@gmail.com>
2019-11-08 14:01:59 +0100

    atuh service

---
+++ Webapp/src/app/Services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {IMembre} from '../Interfaces/imembre';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  public isLogged: boolean = false;
+  public currentUser: IMembre = undefined;
+
+  constructor() { }
+
+  setCurrentUser(user: IMembre){
+    this.currentUser = user;
+    this.isLogged = true;
+  }
+}
