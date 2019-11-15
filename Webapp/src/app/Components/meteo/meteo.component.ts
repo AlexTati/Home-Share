7abbc548d30529da9dd4950837079d7c83ead4d9
s4my <samseche@gmail.com>
2019-11-15 10:49:51 +0100

    Meteo Widget - fix

--- Webapp/src/app/Components/meteo/meteo.component.ts
+++ Webapp/src/app/Components/meteo/meteo.component.ts
@@ -9,7 +9,18 @@ import {migrateLegacyGlobalConfig} from '@angular/cli/utilities/config';
 })
 export class MeteoComponent implements OnInit {
 
-  @Input() cityName: string;
+  @Input() set location (l : string[]){
+    if (l[0] == null) return
+
+    this.http.get<any>('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=f42afff522464314bed135825191211&q=' +
+      l[0] + ',' + l[1] + '&format=json&num_of_days=1&lang=fr').subscribe( data => {
+      this.imgSrc = data.data.current_condition[0].weatherIconUrl[0].value;
+      this.rCity = data.data.request[0].query;
+      this.rTemp = data.data.current_condition[0].temp_C;
+      this.rDesc = data.data.current_condition[0].lang_fr[0].value;
+    })
+
+  }
 
   private rCity;
   private rTemp;
@@ -19,19 +30,7 @@ export class MeteoComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    if (this.cityName !== undefined) {
-      this.http.get<any>('http://api.worldweatheronline.com/premium/v1/weather.ashx?key=f42afff522464314bed135825191211&q=' +
-        this.cityName + '&format=json&num_of_days=1&lang=fr').subscribe( data => {
-        this.imgSrc = data.data.current_condition[0].weatherIconUrl[0].value;
-        this.rCity = data.data.request[0].query;
-        this.rTemp = data.data.current_condition[0].temp_C;
-        this.rDesc = data.data.current_condition[0].lang_fr[0].value;
 
-      })
-    }
-    else{
-      console.log('[METEO WIDGET] error : "cityName" must be defined');
-    }
   }
 
 }
